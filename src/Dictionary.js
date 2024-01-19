75cbf71f3cd6653c6464ea218984a0a7c144a44c
KendraLa <kendralambillon@gmail.com>
2024-01-19 15:21:53 +0100

    Set Dictionary Components

---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './Dictionary.css';
+
+export default function Dictionary() {
+    let [keyword, setKeyword] = useState("");
+
+    function search (e) {
+        e.preventDefault();
+        alert(`Searching for ${keyword}`);
+    }
+
+    function handleKeywordChange(e) {
+        setKeyword(e.target.value);
+    }
+
+    return (
+        <div className="Dictionary">
+            <form onSubmit={search}>
+                <input type="search" onChange={handleKeywordChange} />
+            </form>
+        </div>
+    );
+}
+
