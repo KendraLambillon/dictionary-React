5be59d144ebc68a0d15b0958d832f89d03ac4ecc
KendraLa <kendralambillon@gmail.com>
2024-01-21 00:01:02 +0100

    Netlify link modified

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
           <Dictionary />
         </main>
         <footer className="App-footer">
-          <small>Coded by <a href="http://www.linkedin.com/in/kendra-lambillon/" >Kendra Lambillon</a> with the <a href="https://github.com/KendraLambillon/dictionary-React" >Open-sourced on Github</a> and <a href="https://rococo-sable-51a915.netlify.app" >hosted on Netlify</a>.</small>
+          <small>Coded by <a href="http://www.linkedin.com/in/kendra-lambillon/" >Kendra Lambillon</a> with the <a href="https://github.com/KendraLambillon/dictionary-React" >Open-sourced on Github</a> and <a href="https://dazzling-granita-4b5c7e.netlify.app" >hosted on Netlify</a>.</small>
         </footer>
       </div>
     </div>
